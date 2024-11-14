ec1d5c1cb223a18e5c90e7c448fbed9f9203abb8
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-14 14:24:41 +0530

    canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Canvas from "./Canvas";
+import data from "./data";
 
 const App = () => {
-  return <div className="w-full min-h-screen bg-black text-white"></div>;
+  return (
+    <>
+      <div className="w-full relative  bg-black text-white">
+        {data[0].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
