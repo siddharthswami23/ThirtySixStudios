2fdeaf603a6a15e6b96c7349beab85b542dcb8e5
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-14 15:12:17 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import React, { useEffect } from "react";
 import Canvas from "./Canvas";
 import data from "./data";
 import LocomotiveScroll from "locomotive-scroll";
-
 const App = () => {
   useEffect(() => {
     const locomotiveScroll = new LocomotiveScroll();
   }, []);
-
   return (
     <div className="w-full relative">
       <div className="w-full min-h-screen bg-red-500 text-white">
@@ -15,25 +13,7 @@ const App = () => {
           <Canvas key={index} details={canvasdets} />
         ))}
       </div>
-
-      <div className="w-full min-h-screen bg-blue-500 text-white">
-        {data[1].map((canvasdets, index) => (
-          <Canvas key={index} details={canvasdets} />
-        ))}
-      </div>
-
-      <div className="w-full min-h-screen bg-green-400 text-white">
-        {data[2].map((canvasdets, index) => (
-          <Canvas key={index} details={canvasdets} />
-        ))}
-      </div>
-      <div className="w-full min-h-screen bg-lime-400 text-white">
-        {data[3].map((canvasdets, index) => (
-          <Canvas key={index} details={canvasdets} />
-        ))}
-      </div>
     </div>
   );
 };
-
 export default App;
